4896cd46013faf80a94fbbcfb2b2baf411adc8cd
ema002 <ema.paunovic.ema@gmail.com>
2023-04-08 21:27:58 +0200

    UI first

--- warmup/FirstApp/AwesomeApp/AwesomeApp.Android/obj/Debug/100/android/src/crc64a8a173fc3f8c46da/MainActivity.java
+++ warmup/FirstApp/AwesomeApp/AwesomeApp.Android/obj/Debug/100/android/src/crc64a8a173fc3f8c46da/MainActivity.java
@@ -19,18 +19,16 @@ public class MainActivity
 	public MainActivity ()
 	{
 		super ();
-		if (getClass () == MainActivity.class) {
+		if (getClass () == MainActivity.class)
 			mono.android.TypeManager.Activate ("AwesomeApp.Droid.MainActivity, AwesomeApp.Android", "", this, new java.lang.Object[] {  });
-		}
 	}
 
 
 	public MainActivity (int p0)
 	{
 		super (p0);
-		if (getClass () == MainActivity.class) {
+		if (getClass () == MainActivity.class)
 			mono.android.TypeManager.Activate ("AwesomeApp.Droid.MainActivity, AwesomeApp.Android", "System.Int32, mscorlib", this, new java.lang.Object[] { p0 });
-		}
 	}
 
 
